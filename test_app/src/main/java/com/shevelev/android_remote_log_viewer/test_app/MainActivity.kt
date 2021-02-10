5cd4d025e95fec15c6fe499d89a4bce479f42e74
Alex Shevelev <al.e.shevelev@gmail.com>
2021-02-10 19:52:04 +0300

    Test logcat output has been added

--- test_app/src/main/java/com/shevelev/android_remote_log_viewer/test_app/MainActivity.kt
+++ test_app/src/main/java/com/shevelev/android_remote_log_viewer/test_app/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.shevelev.android_remote_log_viewer.test_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.shevelev.android_remote_log_viewer.test_app.databinding.ActivityMainBinding
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        showLog()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun showLog() {
+        try {
+            val process = Runtime.getRuntime().exec("logcat -d")
+
+            val log = BufferedReader(InputStreamReader(process.inputStream)).use { bufferedReader ->
+                val logBuilder = StringBuilder()
+
+                var line: String?
+                while (bufferedReader.readLine().also { line = it } != null) {
+                    logBuilder.appendLine(line)
+                    logBuilder.appendLine()
+                }
+
+                logBuilder
+            }
+
+            binding.logOutput.text = log.toString()
+        } catch (ex: IOException) {
+            binding.logOutput.text = "Something went wrong: ${ex.message}"
+        }
     }
 }
